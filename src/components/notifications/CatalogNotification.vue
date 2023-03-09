8c07adfd9eb7ad36137a174bb0724e993817a17a
Yurii Rybachok <drummer2009@ukr.net>
2023-03-09 21:00:53 +0200

    add animation to notification

--- src/components/notifications/CatalogNotification.vue
+++ src/components/notifications/CatalogNotification.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="catalog-notification">
-        <div class="catalog-notification__content" v-for="message in messages" :key="message.id">
-            <span>{{ message.name }}</span>
-            <i class="material-icons">check_circle</i>
-        </div>
+        <TransitionGroup
+            name="animate"
+            class="catalog-notification__list"
+            tag="div"
+        >
+            <div class="catalog-notification__content" v-for="message in messages" :key="message.id">
+                <span>{{ message.name }}</span>
+                <i class="material-icons">check_circle</i>
+            </div>
+        </TransitionGroup>
     </div>
 </template>
 
 <script>
+import { TransitionGroup } from 'vue';
 export default {
     name: 'CatalogNotification',
     props: {
@@ -17,6 +24,20 @@ export default {
                 return []
             }
         }
+    },
+    components: {TransitionGroup},
+    methods: {
+        hideNotification() {
+            let vm = this;
+            if(!this.messages.length) {
+                setInterval(function() {
+                    vm.messages.splice(vm.messages.length - 1, 1)
+                }, 3000)
+            }
+        }
+    },
+    mounted() {
+        this.hideNotification()
     }
 }
 </script>
@@ -43,5 +64,25 @@ export default {
         gap: 10px;
         font-size: 12px;
     }
+}
+.animate {
+   &-enter-from {
+    transform: translateX(120px);
+    opacity: 0;
+   } 
+   &-enter-active {
+    transition: all .6s ease;
+   }
+   &-leave-active {
+    transition: transform .6s ease, opacity .6s, height .6s .2s;
+   }
+   &-leave-to {
+    height: 0;
+    transform: translateX(120px);
+    opacity: 0;
+   }
+   &-move {
+    transition: transform .6s ease;
+   }
 }
 </style>
